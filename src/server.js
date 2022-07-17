958e6f1709327a23c02215524bdee1e9b7f1f60f
Michellah <hei.michellah@gmail.com>
2022-07-17 19:25:33 +0200

    modification du port

--- src/server.js
+++ src/server.js
@@ -33,7 +33,7 @@ app.get("/:id", (req,res)=>{
 })
 
 const { PORT=3000, LOCAL_ADDRESS='0.0.0.0' } = process.env
-server.listen(PORT, LOCAL_ADDRESS, () => {
+app.listen(PORT, LOCAL_ADDRESS, () => {
   const address = server.address();
   console.log('server listening at', address);
 });
